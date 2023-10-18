60b3d6d947e002efd9fad111d8660fdb38ceffe8
Roman Nekrasov <33453661+RomanNekrasov@users.noreply.github.com>
2023-10-18 09:22:25 +0200

    Add files via upload

---
+++ components/classifier/src/component.py
@@ -0,0 +1,94 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix, hstack
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from joblib import dump, load
+from google.cloud import storage
+import pandas as pd
+from pathlib import Path
+import json
+import os
+import argparse
+
+
+# create a function to add features
+def add_feature(X, feature_to_add):
+    '''
+    Returns sparse feature matrix with added feature.
+    feature_to_add can also be a list of features.
+    '''
+    return hstack([X, csr_matrix(feature_to_add).T], 'csr')
+
+
+def train_multilabel_classifier(project_id, feature_path, model_repo, metrics_path):
+    # load the training data
+    data = pd.read_csv(feature_path, index_col=None)
+    X = data['comment_text']
+    y_all = data.drop('comment_text', axis=1)
+    cols_target = y.columns
+
+    # vectorize the text data
+    vect = TfidfVectorizer(max_features=5000,stop_words='english')
+    X_dtm = vect.fit_transform(X)
+
+    # set up classifier
+    logreg = LogisticRegression(C=12.0)
+
+    for labelnum, label in enumerate(cols_target):
+        print('... Processing {}'.format(label))
+        y = y_all[label]
+        # train the model using X_dtm & y
+        logreg.fit(X_dtm,y)
+
+        # save the model
+        local_file = f'/tmp/{labelnum}_{label}_model.joblib'
+        dump(logreg, local_file)
+
+        # Save to GCS as f'{labelnum}_{label}_model.joblib'
+        client = storage.Client(project=project_id)
+        bucket = client.get_bucket(model_repo)
+        blob = bucket.blob(f'{labelnum}_{label}_model.joblib')
+
+        # Upload the locally saved model
+        blob.upload_from_filename(local_file)
+
+        # Cleaning up by deleting the local file
+        os.remove(local_file)
+
+        # make predictions with training data
+        y_pred_X = logreg.predict(X_dtm)
+        # compute the evalution metrics for the training data
+        metrics = {
+            'accuracy': accuracy_score(y,y_pred_X),
+            'precision': 'TODO',
+            'recall': 'TODO',
+            'f1': 'TODO'
+        }
+        print(metrics)
+
+        # save the evaluation metrics
+        Path(metrics_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(metrics_path, 'w') as outfile:
+            json.dump(metrics, outfile)
+
+        # chain current label to X_dtm
+        X_dtm = add_feature(X_dtm, y)
+        print('Shape of X_dtm is now {}'.format(X_dtm.shape))
+        # chain current label predictions to test_X_dtm
+        # test_X_dtm = add_feature(test_X_dtm, test_y)
+        # print('Shape of test_X_dtm is now {}'.format(test_X_dtm.shape))
+
+# Defining and parsing the command-line arguments
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project_id', type=str, help="GCP project id")
+    parser.add_argument('--feature_path', type=str, help="CSV file with features")
+    parser.add_argument('--model_repo', type=str, help="Name of the model bucket")
+    parser.add_argument('--metrics_path', type=str, help="Name of the file to be used for saving evaluation metrics")
+    args = parser.parse_args()
+    return vars(args)
+
+
+if __name__ == '__main__':
+    train_multilabel_classifier(**parse_command_line_arguments())
+    # The *args and **kwargs is a common idiom to allow arbitrary number of arguments to functions
---
+++ components/toxic-data-cleaner/src/component.py
@@ -0,0 +1,34 @@
+from google.cloud import storage
+import pandas as pd
+import os
+from pathlib import Path
+import re
+import logging
+
+# function has to be used for both the training and the prediction data
+def clean_data(filename):
+  # get data
+  df = pd.read_csv(os.path.join(Path, filename))
+
+  def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r"\'scuse", " excuse ", text)
+    text = re.sub('\W', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    text = text.strip(' ')
+    return text
+
+  df['comment_text'] = df['comment_text'].map(lambda com : clean_text(com))
+  df.drop(['id'], axis=1, inplace=True)
+
+  logging.info('Cleaned text!')
+
