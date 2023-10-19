78faccde04d3767f5d62beb0d0136095cbff8d60
Nekrasov <roman.nekrasov@centric.eu>
2023-10-19 22:55:11 +0200

    same typo in different place :D

--- components/toxic-data-ingestor/src/component.py
+++ components/toxic-data-ingestor/src/component.py
@@ -8,12 +8,12 @@ def download_data(project_id, bucket, file_name, feature_path):
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     client = storage.Client(project=project_id)
     bucket = client.get_bucket(bucket)
-    blob = bucket.blob(train_file_name)
+    blob = bucket.blob(file_name)
     # Creating the directory where the output file is created (the directory
     # may or may not exist).
     Path(feature_path).parent.mkdir(parents=True, exist_ok=True)
     blob.download_to_filename(feature_path)
-    logging.info(f'Downloaded {train_file_name}!')
+    logging.info(f'Downloaded {file_name}!')
 
 
 # Defining and parsing the command-line arguments
