a097320e13e701933c7b32ea7c9454808651cfc0
huub <huubvandevoort@home.nl>
2023-10-21 20:07:21 +0200

    Finished the cleaner api component

--- components/toxic-data-cleaner/src/app.py
+++ components/toxic-data-cleaner/src/app.py
@@ -2,11 +2,24 @@
 from flask import Flask, jsonify, request, current_app
 from component import *
 import pickle as pkl
+import joblib
 from google.cloud import storage
+
+
+def load_vectorizor():
+    # load vectorizer from file
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('models_de2023_group1')
+    blob = bucket.blob('vectorizer_model.joblib') # vectorizer.pickle is the name of the file in the bucket
+    blob.download_to_filename('/tmp/vectorizer_model.joblib')
+
+    with open('/tmp/vectorizer_model.joblib', 'rb') as f:
+        vectorizer = joblib.load(f)
+        f.close()
+        return vectorizer
 
 def create_app():
     app = Flask(__name__)
-
     with app.app_context():
         load_vectorizor()
     return app
@@ -14,30 +27,17 @@ def create_app():
 # creating the app
 app = create_app()
 
-
-def load_vectorizor():
-    # load vectorizer from file
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket('models_de2023_group1')
-    blob = bucket.blob('vectorizer.pickle') # vectorizer.pickle is the name of the file in the bucket
-    blob.download_to_filename('/tmp/vectorizer.pickle')
-
-    with open('/tmp/vectorizer.pickle', 'rb') as f:
-        vectorizer = pkl.load(f)
-        f.close()
-        current_app.vectorizer = vectorizer
-
-
 # A decorator used to tell the application which URL is associated function
 # the complete URL will be http://ip:port/users?name=some_value
 @app.route('/', methods=["POST"])
 def clean_data():
+    vectorizer = load_vectorizor()
     text = request.json.get('text')
     if not text:
         return jsonify(error="Please provide a 'text' field in the request body."), 400
     cleaned_text = clean_text(text)
-    cleaned_data = current_app.vectorizor.transform([cleaned_text])
-    return jsonify(text=cleaned_data) # returning the cleaned text in json format
+    cleaned_data = vectorizer.transform([cleaned_text])
+    return jsonify(vector=str(cleaned_data)) # returning the cleaned text in json format
 
 
 # The code within this conditional block will only run the python file is executed as a
